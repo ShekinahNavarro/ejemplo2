d83938836ef168cd63a14fe9c5f1befcfb445402
shekinahNavarro <navarroshekinah31@gmail.com>
2023-12-02 10:23:15 -0500

    ejemplo js

---
+++ 1.js
@@ -0,0 +1,47 @@
+/*for (let i= 1; i <= 10; i++) {
+    alert(i)
+}
+*/
+/*
+for (let i= 10; i>= 1; i--){
+    alert(i)
+}
+*/
+/*
+for (let i= 10; i< 110; i+=10){
+    alert(i)
+}
+*/
+//LLENAR UNA CADENA DE TEXTO CON BUCLE
+/*
+let texto="";
+for (let i= 1; i<= 10;i++){
+    texto += i;
+}
+console.log(texto)
+*/
+// falta completar
+/*let num= 0;
+for(let i= 1; i<=10;i++){
+    console.log('numero->${num}');
+    console.log('iterador->');
+}
+*/
+/*
+let texto="";
+for (let i= 1; i <= 10; i++){
+    texto += i+"<br />";
+}
+document.getElementById("demo").innerHTML=texto;
+*/
+//ejemplo
+/*
+let texto="";
+const cars =["BMW","Volvo","Saab","Ford","Fiat","Audi"];
+for (let i= 0; i < cars.length;i++){
+    texto += cars[i]+"<br />";
+}
+document.getElementById("demo").innerHTML=texto;
+*/
+
+
